02fe2fe9b8076d502585755681eb5602a50cda08
toniocodo <antoine@originprotocol.com>
2024-07-22 14:31:25 +0200

    feat: add simulation, disabled button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,95 +1,93 @@
-import { useState, useEffect } from 'react'
-import type { MetaFunction } from '@remix-run/cloudflare'
-import { ConnectButton } from '~/components/ConnectButton'
-import { parseAbi, zeroAddress } from 'viem'
+import { useState, useEffect } from "react";
+import type { MetaFunction } from "@remix-run/cloudflare";
+import { ConnectButton } from "~/components/ConnectButton";
+import { parseAbi, zeroAddress } from "viem";
 import {
   useAccount,
   useReadContracts,
   useWriteContract,
   useWaitForTransactionReceipt,
-  UseWriteContractReturnType
-} from 'wagmi'
+  UseWriteContractReturnType,
+  useSimulateContract,
+} from "wagmi";
 
-import { formatEth } from '~/utils/bigint'
-import { Modal } from '~/components/Modal'
-import { OriginLogo } from '~/components/Icons'
+import { formatEth } from "~/utils/bigint";
+import { Modal } from "~/components/Modal";
+import { OriginLogo } from "~/components/Icons";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'Unstake OGN' },
-    { name: 'description', content: 'Unstake your OGN' }
-  ]
-}
+    { title: "Unstake OGN" },
+    { name: "description", content: "Unstake your OGN" },
+  ];
+};
 
-const storyAddress = '0xCcE8E784c777fb9435F89f4E45f8b7FC49f7669f'
+const storyAddress = "0xCcE8E784c777fb9435F89f4E45f8b7FC49f7669f";
 const abi = parseAbi([
   `function balanceOf(address userAddress) view returns (uint256)`,
   `function latestStakeTime(address userAddress) view returns (uint256)`,
-  `function unstake() returns (uint256)`
-])
+  `function unstake() returns (uint256)`,
+]);
 
-const legacyAddress = '0x501804b374ef06fa9c427476147ac09f1551b9a0'
+const legacyAddress = "0x501804b374ef06fa9c427476147ac09f1551b9a0";
 const legacyAbi = parseAbi([
   `function totalCurrentHoldings(address userAddress) view returns (uint256)`,
-  `function exit()`
-])
-
-const blockNumber = undefined
-// const blockNumber = 19405119n
-// const address = '0x415bd9A5e2fDcB8310ceE3F785F25B5E4D4564E3'
+  `function exit()`,
+]);
 
 export default function Index() {
-  const [isOpen, setIsOpen] = useState(false)
-  const { isConnected, address } = useAccount()
+  const [isOpen, setIsOpen] = useState(false);
+  const { isConnected, address } = useAccount();
 
   const { data, isLoading, refetch } = useReadContracts({
-    blockNumber,
     contracts: [
       {
         abi,
         address: storyAddress,
-        functionName: 'balanceOf',
-        args: [address || zeroAddress]
+        functionName: "balanceOf",
+        args: [address || zeroAddress],
       },
       {
         abi: legacyAbi,
         address: legacyAddress,
-        functionName: 'totalCurrentHoldings',
-        args: [address || zeroAddress]
+        functionName: "totalCurrentHoldings",
+        args: [address || zeroAddress],
         // args: ['0x415bd9A5e2fDcB8310ceE3F785F25B5E4D4564E3']
-      }
+      },
     ],
     query: {
-      enabled: isConnected
-    }
-  })
+      enabled: isConnected,
+    },
+  });
 
-  const storyStake = data?.[0].result || 0n
-  const legacyStake = data?.[1].result || 0n
+  const storyStake = data?.[0].result || 0n;
+  const legacyStake = data?.[1].result || 0n;
 
-  const contractWrite = useWriteContract()
-  const txReceipt = useWaitForTransactionReceipt({ hash: contractWrite.data })
+  const contractWrite = useWriteContract();
+  const txReceipt = useWaitForTransactionReceipt({ hash: contractWrite.data });
 
   useEffect(() => {
-    if (contractWrite.status === 'pending') {
-      setIsOpen(true)
+    if (contractWrite.status === "pending") {
+      setIsOpen(true);
     }
-  }, [contractWrite.status, txReceipt.data, refetch])
+  }, [contractWrite.status, txReceipt.data, refetch]);
 
-  let modalTitle = 'Transaction in process'
-  let modalStatus = 'loading'
-  let modalDescription
+  let modalTitle = "Transaction in process";
+  let modalStatus = "loading";
+  let modalDescription;
 
-  if (contractWrite.status === 'pending') {
-    modalTitle = 'Please check your wallet'
-  } else if (contractWrite.status === 'success' && txReceipt.data) {
-    modalTitle = 'Transaction successful'
-    modalStatus = 'success'
+  if (contractWrite.status === "pending") {
+    modalTitle = "Please check your wallet";
+  } else if (contractWrite.status === "success" && txReceipt.data) {
+    modalTitle = "Transaction successful";
+    modalStatus = "success";
   } else if (contractWrite.error) {
-    modalTitle = 'Transaction failed'
-    modalStatus = 'error'
+    modalTitle = "Transaction failed";
+    modalStatus = "error";
     modalDescription =
-      contractWrite.error.shortMessage || contractWrite.error.message
+      "shortMessage" in contractWrite.error
+        ? contractWrite.error.shortMessage
+        : contractWrite.error.message;
   }
 
   return (
@@ -117,11 +115,7 @@ export default function Index() {
         ) : (
           <>
             {address && storyStake > 0n && (
-              <StoryStake
-                address={address}
-                balance={storyStake}
-                write={contractWrite}
-              />
+              <StoryStake balance={storyStake} write={contractWrite} />
             )}
             {legacyStake > 0n && (
               <LegacyStake balance={legacyStake} write={contractWrite} />
@@ -135,27 +129,25 @@ export default function Index() {
         txLink={
           contractWrite.data
             ? `https://etherscan.io/tx/${contractWrite.data}`
-            : ''
+            : ""
         }
         title={modalTitle}
         isOpen={isOpen}
         setIsOpen={() => {
-          setIsOpen(false)
-          refetch()
+          setIsOpen(false);
+          refetch();
         }}
       />
     </div>
-  )
+  );
 }
 
 const StoryStake = ({
-  address,
   balance,
-  write
+  write,
 }: {
-  address: `0x${string}`
-  balance: bigint
-  write: UseWriteContractReturnType
+  balance: bigint;
+  write: UseWriteContractReturnType;
 }) => {
   // const { data } = useReadContracts({
   //   contracts: [
@@ -171,6 +163,14 @@ const StoryStake = ({
   // const latestStakeTime = Number(data?.[0].result || 0n) || Date.now() / 1000
   // const daysStaked = (Date.now() / 1000 - latestStakeTime) / (60 * 60 * 24)
 
+  const { data, isLoading } = useSimulateContract({
+    abi,
+    address: storyAddress,
+    functionName: "unstake",
+  });
+
+  const disabled = !data?.request || isLoading;
+
   return (
     <div className="white-box flex flex-col items-center gap-1">
       <div>Story Staked OGN:</div>
@@ -180,44 +180,50 @@ const StoryStake = ({
         {daysStaked.toLocaleString(undefined, { maximumFractionDigits: 0 })}
       </div> */}
       <button
-        className="btn py-2 px-6 mt-4"
+        className={`${disabled ? "btn-disabled" : "btn"} py-2 px-6 mt-4`}
+        disabled={disabled}
         onClick={() => {
-          write.writeContract({
-            abi,
-            address: storyAddress,
-            functionName: 'unstake'
-          })
+          if (data?.request) {
+            write.writeContract(data.request);
+          }
         }}
       >
         Unstake
       </button>
     </div>
-  )
-}
+  );
+};
 
 const LegacyStake = ({
   balance,
-  write
+  write,
 }: {
-  balance: bigint
-  write: UseWriteContractReturnType
+  balance: bigint;
+  write: UseWriteContractReturnType;
 }) => {
+  const { data, isLoading } = useSimulateContract({
+    abi: legacyAbi,
+    address: legacyAddress,
+    functionName: "exit",
+  });
+
+  const disabled = !data?.request || isLoading;
+
   return (
     <div className="white-box flex flex-col items-center gap-1">
       <div>Legacy Staked OGN:</div>
       <div className="font-medium text-2xl mb-4">{formatEth(balance)}</div>
       <button
-        className="btn py-2 px-6"
+        className={`${disabled ? "btn-disabled" : "btn"} py-2 px-6`}
+        disabled={disabled}
         onClick={() => {
-          write.writeContract({
-            abi: legacyAbi,
-            address: legacyAddress,
-            functionName: 'exit'
-          })
+          if (data?.request) {
+            write.writeContract(data.request);
+          }
         }}
       >
         Unstake
       </button>
     </div>
-  )
-}
+  );
+};
